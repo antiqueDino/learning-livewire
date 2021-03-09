6b3479370795f036db27c1b315ced228cee3e3b1
antiqueDino <dinogbaguidi@gmail.com>
2021-03-09 15:28:24 +0100

    modifiying asset full url with origin in livewire php

--- config/livewire.php
+++ config/livewire.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'asset_url' => url('/'),
+    'asset_url' => 'https://learning-livewire.herokuapp.com/?active=true&sortAsc=true',
 
     /*
     |--------------------------------------------------------------------------
