7bada45915512ab14016542de203e83b3d9f90ed
antiqueDino <dinogbaguidi@gmail.com>
2021-03-09 15:57:03 +0100

    removing js asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livewire Examples</title>
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}" ></script>
+    <link href="/css/main.css" rel="stylesheet">
     
     @livewireStyles()
 
