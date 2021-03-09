b7bc3a1d82a9a675813bf31dc51c2c8bb1dbf552
antiqueDino <dinogbaguidi@gmail.com>
2021-03-09 03:01:35 +0100

    Remove defer in script app-blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>Livewire Examples</title>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" ></script>
     
     @livewireStyles
 
